52a281c16db76fb51aa5d3cfd4cd74f9fd921959
Wildson Seguchi <wseguchi@gmail.com>
2022-08-03 21:31:41 -0300

    Created all App components and UI basic interface. Created Global Context.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import { AddTransaction } from './AddTransaction';
 import './App.css';
+import { Balance } from './Balance';
+import { Header } from './Header.js';
+import { IncomeExpenses } from './IncomeExpenses';
+import { TransactionList } from './TransactionList';
+import { GlobalProvider } from './context/GlobalState';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>Expense Tracker</header>
-    </div>
+    <GlobalProvider>
+      <Header />
+      <div id='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
   );
 }
 
